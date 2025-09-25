2cb170dc3b73aec346e2f43cd01854cfab35c356
Dcoo <davidcoo@me.com>
2025-09-25 15:16:28 -0400

    Document Vercel project linking process

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+
+export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    runtime: 'nodejs20.x',
+  }),
+  site: 'https://example.vercel.app',
+  integrations: [],
+});
